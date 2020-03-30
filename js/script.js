c263ebbb931f8df993e579cb2abf60074c5411d2
Elias <obedelias01@gmail.com>
2020-03-30 19:06:00 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,15 +13,15 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
 
     var dc = {};
 
-    var homeHtmlUrl = "snippets/home-snippet.html";
+    var homeHtmlUrl = "Snippets/home-snippet.html";
     var allCategoriesUrl =
         "https://davids-restaurant.herokuapp.com/categories.json";
-    var categoriesTitleHtml = "snippets/categories-title-snippet.html";
-    var categoryHtml = "snippets/category-snippet.html";
+    var categoriesTitleHtml = "Snippets/categories-title-snippet.html";
+    var categoryHtml = "Snippets/category-snippet.html";
     var menuItemsUrl =
         "https://davids-restaurant.herokuapp.com/menu_items.json?category=";
-    var menuItemsTitleHtml = "snippets/menu-items-title.html";
-    var menuItemHtml = "snippets/menu-item.html";
+    var menuItemsTitleHtml = "Snippets/menu-items-title.html";
+    var menuItemHtml = "Snippets/menu-item.html";
 
 // Convenience function for inserting innerHTML for 'select'
     var insertHtml = function (selector, html) {
